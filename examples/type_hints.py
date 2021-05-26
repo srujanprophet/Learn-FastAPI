30b9b74e75f77c877767b6d7c57e4f7fad9b4e60
Srujan <srujanprophet@gmail.com>
2021-05-26 10:22:20 +0200

    Add types to type hints example

--- examples/type_hints.py
+++ examples/type_hints.py
@@ -1,7 +1,10 @@
 """ Example script for python 'type hints' """
 
-def get_full_name(first_name, last_name):
+
+def get_full_name(first_name: str, last_name: str):
+    """ Return full name given first and last name"""
     full_name = first_name.title() + " " + last_name.title()
     return full_name
+
 
 print(get_full_name("zack", "hemsey"))
